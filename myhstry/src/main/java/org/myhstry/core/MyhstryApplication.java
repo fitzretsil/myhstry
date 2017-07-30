e41310a29b3071c6cb9328b01e83fe24c850eb3e
fitzretsil <fitzretsil@gmail.com>
2017-07-30 11:00:10 +0100

    Initial commit

---
+++ myhstry/src/main/java/org/myhstry/core/MyhstryApplication.java
@@ -0,0 +1,12 @@
+package org.myhstry.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhstryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhstryApplication.class, args);
+	}
+}
