dac0201b9fffeba00561ccf41e3f65cf7492ae71
Andy Diack <fitzretsil@gmail.com>
2018-08-27 23:58:48 +0100

    Thymeleaf Layout
    As a developer I do not want to recreate the layout elements for each
    page

--- src/main/java/org/myhstry/core/WebSecurityConfig.java
+++ src/main/java/org/myhstry/core/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
+		http.authorizeRequests().antMatchers("/", "/home", "/style/*.css", "/images/*.*").permitAll().anyRequest().authenticated().and().formLogin()
 				.loginPage("/login").permitAll().and().logout().permitAll();
 	}
 	
